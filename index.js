befb310f82d8c97a33cc0c150651459a9d03a9b4
Sackett <skeesen8@gmail.com>
2023-09-13 15:43:06 -0500

    finished

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(){
+    return (2000 / 100)
+}
+// const square => (multiplyBySelf){ 
+//     return multiplyBySelf * multiplyBySelf
+
+// }
+
+const square = (multiplyBySelf) => 
+(multiplyBySelf * multiplyBySelf)
+
+const add = (num1, num2) => num1 + num2
